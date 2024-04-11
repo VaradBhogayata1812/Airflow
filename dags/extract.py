db6f6e1876e74ed1881116507d34bb00d502f838
VaradBhogayata1812 <146085585+VaradBhogayata1812@users.noreply.github.com>
2024-04-11 16:00:49 +0100

    Update extract.py

--- dags/extract.py
+++ dags/extract.py
@@ -6,12 +6,12 @@ from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFile
 # Define the default DAG arguments
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['varadbhogayata78@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': days_ago(1),
 }
 
 # Define the DAG, its schedule, and set it to be active
@@ -27,9 +27,7 @@ with DAG(
     # Task to download log files from GCS to the local filesystem
     download_logs = GCSToLocalFilesystemOperator(
         task_id='download_logs',
-        bucket='your-bucket-name-here',
-        object_name='path/to/logfiles/*', # Use wildcard to download all files in the folder
-        filename='/path/to/destination/on/local/filesystem/{{ ds }}', # Using execution date to organize files
+        bucket='rawlogfiles',
+        object_name='logfiles/W3SVC1/', # Use wildcard to download all files in the folder
+        filename='Airflow/gsc/data', # Using execution date to organize files
     )
-
-# No need to set explicit dependencies as there's only one task
