489a041de8f2ffaad58aee45cd8253873192a1a5
VaradBhogayata1812 <146085585+VaradBhogayata1812@users.noreply.github.com>
2024-04-22 11:41:19 +0100

    Update load.py

--- dags/load.py
+++ dags/load.py
@@ -1,123 +1,3 @@
-# # from airflow import DAG
-# # from airflow.utils.dates import days_ago
-# # from datetime import timedelta
-# # from airflow.operators.python import PythonOperator
-# # from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator, BigQueryInsertJobOperator
-# # from google.cloud import storage
-
-# # default_args = {
-# #     'owner': 'airflow',
-# #     'start_date': days_ago(1),
-# #     'email': ['varadbhogayata78@gmail.com'],
-# #     'email_on_failure': True,
-# #     'email_on_retry': True,
-# #     'retries': 1,
-# #     'retry_delay': timedelta(minutes=5),
-# # }
-
-# # BUCKET_NAME = 'transformedlogfiles'
-# # GCS_PATH = 'transformed_logs/'
-# # LOCAL_TRANSFORMED_PATH = '/opt/airflow/transformed/W3SVC1/'
-# # DATASET_NAME = 'loadeddata'
-# # TABLE_NAME = 'loadedlogfiles'
-
-# # with DAG(
-# #     'load_logs_to_bigquery',
-# #     default_args=default_args,
-# #     description='A DAG to load log files to BigQuery',
-# #     schedule_interval=timedelta(days=1),
-# #     catchup=False,
-# #     tags=['etl', 'load'],
-# # ) as dag:
-    
-# #     create_table_task = BigQueryCreateEmptyTableOperator(
-# #         task_id='create_bigquery_table',
-# #         dataset_id=DATASET_NAME,
-# #         table_id=TABLE_NAME,
-# #         exists_ok=True,
-# #     )
-
-# #     load_to_bq_task = BigQueryInsertJobOperator(
-# #         task_id='load_to_bigquery',
-# #         configuration={
-# #             'load': {
-# #                 'sourceUris': [f'gs://{BUCKET_NAME}/{GCS_PATH}*.csv'],
-# #                 'destinationTable': {
-# #                     'projectId': 'etl-project-418923',
-# #                     'datasetId': DATASET_NAME,
-# #                     'tableId': TABLE_NAME,
-# #                 },
-# #                 'sourceFormat': 'CSV',
-# #                 'writeDisposition': 'WRITE_APPEND',
-# #                 'autodetect': True,
-# #             },
-# #         },
-# #     )
-
-# #     create_table_task >> load_to_bq_task
-
-
-
-
-
-
-# from airflow import DAG
-# from airflow.utils.dates import days_ago
-# from datetime import timedelta
-# from airflow.providers.google.cloud.operators.bigquery import (
-#     BigQueryCreateEmptyTableOperator,
-#     BigQueryInsertJobOperator
-# )
-
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': days_ago(1),
-#     'email': ['varadbhogayata78@gmail.com'],
-#     'email_on_failure': True,
-#     'email_on_retry': True,
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5),
-# }
-
-# BUCKET_NAME = 'transformedlogfiles'
-# GCS_PATH = 'transformed_logs/'
-# DATASET_NAME = 'loadeddata'
-# TABLE_NAME = 'loadedlogfiles'
-
-# with DAG(
-#     'load_logs_to_bigquery',
-#     default_args=default_args,
-#     description='A DAG to load log files to BigQuery',
-#     schedule_interval=timedelta(days=1),
-#     catchup=False,
-#     tags=['load'],
-# ) as dag:
-
-#     create_table_task = BigQueryCreateEmptyTableOperator(
-#         task_id='create_bigquery_table',
-#         dataset_id=DATASET_NAME,
-#         table_id=TABLE_NAME,
-#         exists_ok=True,
-#     )
-
-#     load_to_bq_task = BigQueryInsertJobOperator(
-#         task_id='load_to_bigquery',
-#         configuration={
-#             'load': {
-#                 'sourceUris': [f'gs://{BUCKET_NAME}/{GCS_PATH}*.csv'],
-#                 'destinationTable': {
-#                     'projectId': 'etl-project-418923',
-#                     'datasetId': DATASET_NAME,
-#                     'tableId': TABLE_NAME,
-#                 },
-#                 'sourceFormat': 'CSV',
-#                 'writeDisposition': 'WRITE_TRUNCATE',  # Overwrites the table
-#                 'autodetect': True,
-#             },
-#         },
-#     )
-
-#     create_table_task >> load_to_bq_task
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from datetime import timedelta
@@ -139,28 +19,29 @@ DATASET_NAME = 'loadeddata'
 TABLE_NAME = 'loadedlogfiles'
 
 with DAG(
-    'load_logs_to_bigquery',
+    'load_transformed_logs_to_bigquery',
     default_args=default_args,
-    description='A DAG to load log files to BigQuery',
+    description='Load transformed log files into Google BigQuery.',
     schedule_interval=timedelta(days=1),
     catchup=False,
-    tags=['load'],
+    tags=['bigquery', 'logs'],
 ) as dag:
 
-    create_table_task = BigQueryCreateEmptyTableOperator(
-        task_id='create_bigquery_table',
+    setup_bigquery_table = BigQueryCreateEmptyTableOperator(
+        task_id='setup_bq_table',
         dataset_id=DATASET_NAME,
         table_id=TABLE_NAME,
-        exists_ok=True
+        exists_ok=True,
+        dag=dag,
     )
 
-    load_to_bq_task = BigQueryInsertJobOperator(
-        task_id='load_to_bigquery',
+    load_csv_to_bigquery = BigQueryInsertJobOperator(
+        task_id='load_csv_to_bq',
         configuration={
             'load': {
                 'sourceUris': [f'gs://{BUCKET_NAME}/{GCS_PATH}*.csv'],
                 'destinationTable': {
-                    'projectId': 'etl-project-418923',  # Ensure this is your actual project ID
+                    'projectId': 'etl-project-418923',
                     'datasetId': DATASET_NAME,
                     'tableId': TABLE_NAME,
                 },
@@ -186,7 +67,8 @@ with DAG(
                     ]
                 },
             },
-        }
+        },
+        dag=dag,
     )
 
-    create_table_task >> load_to_bq_task
+    setup_bigquery_table >> load_csv_to_bigquery
